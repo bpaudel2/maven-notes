286ca141afaa3559ee982bcd5d182f67d3fe86c3
bpaudel2 <beekpal@gmail.com>
2019-06-23 11:06:54 -0500

    pushed notes for maven

---
+++ src/main/java/com/bikram/demo/App.java
@@ -0,0 +1,18 @@
+package com.bikram.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public String sample(){
+        return "sample";
+    }
+
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
